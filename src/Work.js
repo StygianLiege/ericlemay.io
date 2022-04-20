90bd9816a0cb60883569094b3bdf0088f3d02c29
Eric Lemay <95881616+StygianLiege@users.noreply.github.com>
2022-04-19 22:57:29 -0400

    Update Work.js
    reverted for functionality

--- src/Work.js
+++ src/Work.js
@@ -14,11 +14,11 @@ const Work = () => {
           integrated with OpenTelemetry tracing to record worker actvity and
           plot it across a series of Chart.js visuals. You can read more about
           Mist{' '}
-          <Link to="/mist-medium" className="work-link" target="_blank" rel="noopener noreferrer">
+          <Link to="/mist-medium" className="work-link">
             here
           </Link>
           , or check out the{' '}
-          <Link to="/mist-github" className="work-link" target="_blank" rel="noopener noreferrer">
+          <Link to="/mist-github" className="work-link">
             repo
           </Link>
           .
